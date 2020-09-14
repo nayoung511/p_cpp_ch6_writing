d2c4516d3b272541b9f9b7b40e08d93834610f9a
nayoung511 <nayoungbae511@gmail.com>
2020-09-14 18:10:45 +0900

    Initial Commit

---
+++ chapter6_writingProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chapter6_writingProgram
+//
+//  Created by 배나영 on 14/9/2020.
+//  Copyright © 2020 Nayoung Bae. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
